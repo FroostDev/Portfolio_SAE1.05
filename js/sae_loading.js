8480e8da723f0fe0a1b401c5e820e645e63e0cc6
Frost <michael.vlesik@icloud.com>
2024-11-12 16:35:13 +0100

    Finished sae page

--- js/sae_loading.js
+++ js/sae_loading.js
@@ -1,6 +1,19 @@
 let sae_number = Object.keys(SAE);
 let title = [];
+let comp = [];
+let zone_sae = ""
 
-for (let i = 0; i <= sae_number.length; i++) {
-    title.push(sae_number[i]["titre"]);
+for (let i = 0; i < sae_number.length; i++) {
+    title.push(SAE[sae_number[i]].titre);
+    comp.push(SAE[sae_number[i]].compétences);
 }
+
+for(let t = 0; t<sae_number.length; t++) {
+    let tmp = ""
+    comp[t].forEach(element => {
+        tmp+='<div class="competences">'+element+'</div>'
+    });
+    zone_sae += '<a href="sae-document.html"><div class="sae"><h3>'+ sae_number[t]+'</h3><p>'+title[t]+'</p>'+tmp+'</div></a>'
+}
+
+document.querySelector(".container-sae").innerHTML = zone_sae
