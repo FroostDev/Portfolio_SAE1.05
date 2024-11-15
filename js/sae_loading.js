ca97c4883c721f2637af9707e661625cf95ceb49
Frost <michael.vlesik@icloud.com>
2024-11-15 09:49:31 +0100

    Fixed error and added comment

--- js/sae_loading.js
+++ js/sae_loading.js
@@ -10,10 +10,22 @@ for (let i = 0; i < sae_number.length; i++) {
 
 for(let t = 0; t<sae_number.length; t++) {
     let tmp = ""
-    comp[t].forEach(element => {
-        tmp+='<div class="competences">'+element+'</div>'
-    });
-    zone_sae += '<a href="sae-document.html"><div class="sae"><h3>'+ sae_number[t]+'</h3><p>'+title[t]+'</p>'+tmp+'</div></a>'
+    if(comp[t].length>1) {
+        let tmp2 =""
+        comp[t].forEach(e => {
+            tmp2+=" | "+e;
+        })
+        
+        tmp+='<div class="competences"><marquee direction="right" scrollamount="5">'+tmp2+'</marquee></div>';
+    }
+    
+    else {
+        comp[t].forEach(element => {
+            tmp+='<div class="competences">'+element+'</div>'
+        });
+    }
+
+    zone_sae += '<a href="sae-document.html" class="sae-color"><div class="sae"><h3>'+ sae_number[t]+'</h3><p>'+title[t]+'</p>'+tmp+'</div></a>'
 }
 
 document.querySelector(".container-sae").innerHTML = zone_sae
