892b076a9a2f311e44bcc66400a346ea7c94a821
Frost <michael.vlesik@icloud.com>
2024-11-19 09:06:59 +0100

    Srarted new js file

---
+++ js/sae_document_loading.js
@@ -0,0 +1,3 @@
+let param = new URLSearchParams(location.search);
+let num_sae = param.get("sae")
+
--- js/sae_loading.js
+++ js/sae_loading.js
@@ -25,7 +25,7 @@ for(let t = 0; t<sae_number.length; t++) {
         });
     }
 
-    zone_sae += '<a href="sae-document.html" class="sae-color"><div class="sae"><h3>'+ sae_number[t]+'</h3><p>'+title[t]+'</p>'+tmp+'</div></a>'
+    zone_sae += '<a href="sae-document.html?sae='+sae_number[t]+'" class="sae-color"><div class="sae"><h3>'+ sae_number[t]+'</h3><p>'+title[t]+'</p>'+tmp+'</div></a>'
 }
 
 document.querySelector(".container-sae").innerHTML = zone_sae
