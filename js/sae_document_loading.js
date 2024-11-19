3622b728db5597bd088ab49f5ec3289a1be1369b
Frost <michael.vlesik@icloud.com>
2024-11-19 20:48:18 +0100

    Finished javascript
    Need to test animation

--- js/sae_document_loading.js
+++ js/sae_document_loading.js
@@ -7,8 +7,11 @@ let sae_key = Object.keys(SAE)
 let sae_index = sae_key.indexOf(num_sae)
 let comp_html = ""
 let ac_html = ""
+let ressource_html = ""
 let ac_key = Object.keys(SAE[num_sae].AC)
 let ac_value = Object.values(SAE[num_sae].AC)
+let ressource_key = Object.keys(SAE[num_sae].ressources)
+let ressource_value = Object.values(SAE[num_sae].ressources)
 
 // Je change le contenu de tout ce que je peut faire juste avec un innerHTML
 document.querySelector(".sae-title>h3").innerHTML=sae_key[sae_index]
@@ -26,8 +29,11 @@ for(let i = 0; i<ac_key.length; i++) {
     ac_html += '<p><a href=>'+ac_key[i]+' : '+ac_value[i]+'</a></div>'
 }
 
-
+for(let j = 0; j<ressource_key.length; j++) {
+    ressource_html += '<div class="ressources"><h3>'+ressource_key[j]+'</h3><p>'+ressource_value[j]+'</p></div>'
+}
 
 // j'ajoute les compétence dans leur div
-document.querySelector(".competences").innerHTML=comp_html
-document.querySelector(".AC").innerHTML=ac_html
+document.querySelector(".competences").innerHTML=comp_html;
+document.querySelector(".AC").innerHTML=ac_html;
+document.querySelector(".ressources-zone").innerHTML=ressource_html;
